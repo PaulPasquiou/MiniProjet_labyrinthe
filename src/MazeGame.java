11f5443a6f5b18d26352c6ee407a65216e0b8c81
pasquiop <pasquiop@PC-A102-13.iut-valence.fr>
2016-02-19 16:27:12 +0100

    why?: create method play and MazeGame
    how?:method

--- src/MazeGame.java
+++ src/MazeGame.java
@@ -1,4 +1,23 @@
+/** This class is use to start a new game
+ * @author pasquiop
+ *
+ */
 public class MazeGame
 {
+	/**
+	 * create a new game
+	 */
+	public MazeGame()
+	{
 
+	}
+
+	/**
+	 * start the game
+	 */
+	public void play()
+	{
+		System.out.println("La partie commence.");
+		System.out.println("La partie est finie.");
+	}
 }
